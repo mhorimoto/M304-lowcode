04516b52446ec78b348090be807b703d66a481bd
Masafumim Horimoto on smena8 <masafumi.horimoto@gmail.com>
2023-12-24 02:19:18 +0900

    2.4.0 Sub release

--- main/main.ino
+++ main/main.ino
@@ -19,7 +19,7 @@ void get_mcusr(void) {
   wdt_disable();
 }
 
-char *pgname = "M304 Ver2.4.0D";
+char *pgname = "M304 Ver2.4.0";
 
 #define ELE_UECS      0b00000001
 #define ELE_NODESCAN  0b00000010
