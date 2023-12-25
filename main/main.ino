9458854179a06ea5566349af6223e527d62ff55a
Masafumim Horimoto on smena8 <masafumi.horimoto@gmail.com>
2023-12-25 10:37:57 +0900

    for development

--- main/main.ino
+++ main/main.ino
@@ -19,7 +19,7 @@ void get_mcusr(void) {
   wdt_disable();
 }
 
-char *pgname = "M304 Ver2.4.0";
+char *pgname = "M304 Ver2.4.0D";
 
 #define ELE_UECS      0b00000001
 #define ELE_NODESCAN  0b00000010
