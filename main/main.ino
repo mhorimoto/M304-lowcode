fa2ed34e00806b3f5859e3ec18be770e702be066
Masafumim Horimoto on smena8 <masafumi.horimoto@gmail.com>
2024-05-06 08:19:34 +0900

    v3.x.xの開始

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,4 @@
+#define M304V3
 #include <M304.h>
 
 #if _M304_H_V < 1315
@@ -19,7 +20,7 @@ void get_mcusr(void) {
   wdt_disable();
 }
 
-char *pgname = "M304 Ver2.6.0D1";
+char *pgname = "M304 Ver3.0.0D0";
 
 #define ELE_UECS      0b00000001
 #define ELE_NODESCAN  0b00000010
