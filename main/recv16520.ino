09f8e1f460deddff2a90c5b5728399b6c99c0be4
Masafumim Horimoto on smena8 <masafumi.horimoto@gmail.com>
2024-12-07 00:10:04 +0900

    match_rro()を削除。その代わりにcomparison_exp()をrecv16520に移行

--- main/recv16520.ino
+++ main/recv16520.ino
@@ -18,9 +18,13 @@ void UECSupdate16520port(void) {
 			Serial.println("ERR YXML");
 		}
 		float rfval = float(ptr_uecsxmldata->fval);
+		sprintf(lbf,"%d,%d,%d,%s,",ptr_uecsxmldata->room,ptr_uecsxmldata->region,ptr_uecsxmldata->order,
+					ptr_uecsxmldata->type);
+		Serial.print(lbf);
+		Serial.println(ptr_uecsxmldata->fval);
 		for (i=0;i<CCM_TBL_CNT_CMP;i++) {
 			wdt_reset();
-			match_rro(i);
+			//match_rro(i);
 		}
 	}
 }
@@ -31,74 +35,73 @@ void UECSupdate16520port(void) {
 //  結果はflb_cmpope[].resultに代入する
 //  2.3.7D
 //
-void match_rro(int id) {
-  extern uecsM304cmpope flb_cmpope[];
-  extern st_UECSXML *ptr_uecsxmldata;
-  extern byte cmpope_result[];
-  extern char lbf[];
-  extern void sendUECSpacket(int ,char *,int);
-  int  i;
-  char ft[6];
-  float rfval;
-  // 2.3.7DBG
-  sprintf(lbf,"%d,%d,%d,%s,%d",ptr_uecsxmldata->room,ptr_uecsxmldata->region,ptr_uecsxmldata->order,
-  			ptr_uecsxmldata->type,ptr_uecsxmldata->fval);
-  Serial.println(lbf);
-  if (flb_cmpope[id].valid!=0xff) {
-    if (flb_cmpope[id].valid==1) {
-      // ROOM
-      if ((ptr_uecsxmldata->room==0)||(ptr_uecsxmldata->room==flb_cmpope[id].room)) {
-	// REGION
-	if ((ptr_uecsxmldata->region==0)||(ptr_uecsxmldata->region==flb_cmpope[id].region)) {
-	  // ORDER
-	  if ((ptr_uecsxmldata->order==0)||(ptr_uecsxmldata->order==flb_cmpope[id].order)) {
-	    // CCM type
-	    if (!strncmp(ptr_uecsxmldata->type,flb_cmpope[id].ccm_type,19)) {
-	      rfval = float(ptr_uecsxmldata->fval);
-              cmpope_lifecnt[id] = flb_cmpope[id].lifecnt;
-	      switch(flb_cmpope[id].cmpope) {
-	      case R_EQ: // ==
-		if (rfval==flb_cmpope[id].fval) {
-		  cmpope_result[id] = 1;
+/* void match_rro(int id) {
+	extern uecsM304cmpope flb_cmpope[];
+	extern st_UECSXML *ptr_uecsxmldata;
+	extern byte cmpope_result[];
+	extern char lbf[];
+	extern void sendUECSpacket(int ,char *,int);
+	int  i;
+	char ft[6];
+	float rfval;
+
+	if (flb_cmpope[id].valid!=0xff) {
+		// Valid
+		if (flb_cmpope[id].valid==1) {
+			// ROOM
+			if ((ptr_uecsxmldata->room==0)||(ptr_uecsxmldata->room==flb_cmpope[id].room)) {
+				// REGION
+				if ((ptr_uecsxmldata->region==0)||(ptr_uecsxmldata->region==flb_cmpope[id].region)) {
+					// ORDER
+					if ((ptr_uecsxmldata->order==0)||(ptr_uecsxmldata->order==flb_cmpope[id].order)) {
+						// CCM type
+						if (!strncmp(ptr_uecsxmldata->type,flb_cmpope[id].ccm_type,19)) {
+							rfval = float(ptr_uecsxmldata->fval);
+							cmpope_lifecnt[id] = flb_cmpope[id].lifecnt;
+							switch(flb_cmpope[id].cmpope) {
+							case R_EQ: // ==
+								if (rfval==flb_cmpope[id].fval) {
+									cmpope_result[id] = 1;
+								} else {
+									cmpope_result[id] = 0;
+								}
+								break;
+							case R_GT: // >
+								if (rfval>flb_cmpope[id].fval) {
+									cmpope_result[id] = 1;
+								} else {
+									cmpope_result[id] = 0;
+								}
+								break;
+							case R_LT: // <
+								if (rfval<flb_cmpope[id].fval) {
+									cmpope_result[id] = 1;
+								} else {
+									cmpope_result[id] = 0;
+								}
+								break;                                                              
+							case R_GE: // >=
+								if (rfval>=flb_cmpope[id].fval) {
+									cmpope_result[id] = 1;
+								} else {
+									cmpope_result[id] = 0;
+								}
+								break;
+							case R_LE: // <=
+								if (rfval<=flb_cmpope[id].fval) {
+									cmpope_result[id] = 1;
+								} else {
+									cmpope_result[id] = 0;
+								}
+								break;
+							}
+						}
+					}
+				}
+			}
 		} else {
-		  cmpope_result[id] = 0;
-		}
-		break;
-	      case R_GT: // >
-		if (rfval>flb_cmpope[id].fval) {
-		  cmpope_result[id] = 1;
-		} else {
-		  cmpope_result[id] = 0;
-		}
-		break;
-	      case R_LT: // <
-		if (rfval<flb_cmpope[id].fval) {
-		  cmpope_result[id] = 1;
-		} else {
-		  cmpope_result[id] = 0;
-		}
-		break;                                                                     
-	      case R_GE: // >=
-		if (rfval>=flb_cmpope[id].fval) {
-		  cmpope_result[id] = 1;
-		} else {
-		  cmpope_result[id] = 0;
-		}
-		break;
-	      case R_LE: // <=
-		if (rfval<=flb_cmpope[id].fval) {
-		  cmpope_result[id] = 1;
-		} else {
-		  cmpope_result[id] = 0;
-		}
-		break;
-	      }
-	    }
-	  }
-	}
-      }
-    } else {
       //    Serial.println(F("NO MATCH "));
+		}
     }
-  }
 }
+*/
