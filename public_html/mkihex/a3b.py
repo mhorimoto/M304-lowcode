79924b11d3722c36f7bd24178f02f77b7e23899c
M.Horimoto <horimoto@sva001.chun2.ne.jp>
2025-02-13 17:19:17 +0900

    バイト位置修正

--- public_html/mkihex/a3b.py
+++ public_html/mkihex/a3b.py
@@ -2,21 +2,22 @@
 #coding: utf-8
 #
 
-# Version: 3.00
+# Version: 3.10
 # Author: Masafumi Horimoto <mh@ys-lab.tech>
 #
 # Comments:
 # The conditions for relay operation were changed to a four-value setting.
 #    0,N : 0b00: Don't care (Compatibility with older versions)
-#    1,T : 0b01: Both (same as toggle)
-#    2,B : 0b10: Break
-#    3,M : 0b11: Make (Compatibility with older versions)
+#    T   : 0b01: Both (same as toggle)
+#    B   : 0b10: Break
+#    1,M : 0b11: Make (Compatibility with older versions)
 #
 # arguments are as following
-#  id sthr stmn edhr edmn inmn dumn rly(8characters)
+#  id valid sthr stmn edhr edmn inmn dumn rly(8characters)
 
 import sys
 import struct
+Version = "3.20"
 
 #
 # 比較演算子のデータ
@@ -82,11 +83,11 @@ def rly_setting(r,x):
         quit()
     if ((r[x]=="0") or (r[x]=="N")):
         return 0<<s
-    elif ((r[x]=="T") or (r[x]=="1")):
+    elif ((r[x]=="1") or (r[x]=="T")):
         return (0b01<<s)
-    elif ((r[x]=="B") or (r[x]=="2")):
+    elif ((r[x]=="2") or (r[x]=="B")):
         return (0b10<<s)
-    elif ((r[x]=="M") or (r[x]=="3")):
+    elif ((r[x]=="3") or (r[x]=="M")):
         return (0b11<<s)
     else:
         print("ERROR in rly_setting()")
@@ -111,14 +112,15 @@ if __name__ == '__main__':
         flag = True
     else:
         flag = False
-    
+
     if flag == False:
+        print("Version {0}".format(Version))
         print("Usage:")
         print("arguments are as following")
         print(" id valid sthr stmn edhr edmn mnflag inmn dumn rly(8characters) [COND0 [CMP1 COND1 [CMP2 COND2 [CMP3 COND3 [CMP4 COND4]]]]]")
         print("----------------")
         print(" id:")
-        print(" valid: [e(nable)/d(isable)]")
+        print(" valid: e)nable OR d)isable")
         print(" sthr stmn: hour,minute of start")
         print(" edhr edmn: hour,minute of end")
         print(" mnflag: current M fix")
@@ -134,6 +136,10 @@ if __name__ == '__main__':
 
     id = int(args[1])
     valid = args[2]
+    if valid=='e':
+        ih_valid = byte_arrange(1)
+    else: 
+        ih_valid = byte_arrange(255)
     sthr = int(args[3])
     stmn = int(args[4])
     edhr = int(args[5])
@@ -149,10 +155,6 @@ if __name__ == '__main__':
     for x in range(4,8):
         rly_h |= rly_setting(rly,x)
 
-    if valid=='e':
-        ih_valid = byte_arrange(1)
-    else: 
-        ih_valid = byte_arrange(255)
     ih_sthr     = byte_arrange(sthr)
     ih_stmn     = byte_arrange(stmn)
     ih_edhr     = byte_arrange(edhr)
@@ -185,7 +187,10 @@ if __name__ == '__main__':
     
     if argc >= 13:
         ccmid0 = int(args[11])
-        cmpope0 = relope2num[args[12]]
+        if (args[12].isdigit()):
+            cmpope0 = int(args[12])
+        else:
+            cmpope0 = relope2num[args[12]]
         fval0 = float(args[13])
 #        print("          CCMID0={0},CMPOPE0={1},FVAL0={2}".format(ccmid0,cmpope0,fval0))
         ih_ccmid0 = byte_arrange(ccmid0)
@@ -193,9 +198,15 @@ if __name__ == '__main__':
         ih_fval0 = float_arrange(fval0)
         
     if argc >= 17:
-        cmp1 = relope2num[args[14]]
+        if (args[14].isdigit()):
+            cmp1 = int(args[14])
+        else:
+            cmp1 = relope2num[args[14]]
         ccmid1 = int(args[15])
-        cmpope1 = relope2num[args[16]]
+        if (args[16].isdigit()):
+            cmpope1 = int(args[16])
+        else:
+            cmpope1 = relope2num[args[16]]
         fval1 = float(args[17])
 #        print("CMBCMP1={3},CCMID1={0},CMPOPE1={1},FVAL1={2}".format(ccmid1,cmpope1,fval1,cmp1))
         ih_cmp1 = byte_arrange(cmp1)
@@ -203,9 +214,15 @@ if __name__ == '__main__':
         ih_cmpope1 = byte_arrange(cmpope1)
         ih_fval1 = float_arrange(fval1)
     if argc >= 21:
-        cmp2 = relope2num[args[18]]
+        if (args[18].isdigit()):
+            cmp2 = int(args[18])
+        else:
+            cmp2 = relope2num[args[18]]
         ccmid2 = int(args[19])
-        cmpope2 = relope2num[args[20]]
+        if (args[20].isdigit()):
+            cmpope2 = int(args[20])
+        else:
+            cmpope2 = relope2num[args[20]]
         fval2 = float(args[21])
 #        print("CMBCMP2={3},CCMID2={0},CMPOPE2={1},FVAL2={2}".format(ccmid2,cmpope2,fval2,cmp2))
         ih_cmp2 = byte_arrange(cmp2)
@@ -213,9 +230,15 @@ if __name__ == '__main__':
         ih_cmpope2 = byte_arrange(cmpope2)
         ih_fval2 = float_arrange(fval2)
     if argc >= 25:
-        cmp3 = relope2num[args[22]]
+        if (args[22].isdigit()):
+            cmp3 = int(args[22])
+        else:
+            cmp3 = relope2num[args[22]]
         ccmid3 = int(args[23])
-        cmpope3 = relope2num[args[24]]
+        if (args[24].isdigit()):
+            cmpope3 = int(args[24])
+        else:
+            cmpope3 = relope2num[args[24]]
         fval3 = float(args[25])
 #        print("CMBCMP3={3},CCMID3={0},CMPOPE3={1},FVAL3={2}".format(ccmid3,cmpope3,fval3,cmp3))
         ih_cmp3 = byte_arrange(cmp3)
@@ -223,11 +246,14 @@ if __name__ == '__main__':
         ih_cmpope3 = byte_arrange(cmpope3)
         ih_fval3 = float_arrange(fval3)
     if argc >= 29:
-        cmp4 = relope2num[args[26]]
+        if (args[26].isdigit()):
+            cmp4 = int(args[26])
+        else:
+            cmp4 = relope2num[args[26]]
         ccmid4 = int(args[27])
         cmpope4 = relope2num[args[28]]
         fval4 = float(args[29])
-        print("CMBCMP4={3},CCMID4={0},CMPOPE4={1},FVAL4={2}".format(ccmid4,cmpope4,fval4,cmp4))
+#        print("CMBCMP4={3},CCMID4={0},CMPOPE4={1},FVAL4={2}".format(ccmid4,cmpope4,fval4,cmp4))
         ih_cmp4 = byte_arrange(cmp4)
         ih_ccmid4 = byte_arrange(ccmid4)
         ih_cmpope4 = byte_arrange(cmpope4)
