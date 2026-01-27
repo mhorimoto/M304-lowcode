24a30b0e1c862bfffb55234e5a4ae2f05d7c7231
Masafumim Horimoto on smena8 <masafumi.horimoto@gmail.com>
2026-01-28 03:11:05 +0900

    途中

--- main/main.ino
+++ main/main.ino
@@ -21,7 +21,7 @@ void get_mcusr(void) {
     wdt_disable();
 }
 
-char *pgname = "M304 ABCO2-005 ";
+char *pgname = "M304 ABCO2-006 ";
 
 #define ELE_UECS      0b00000001
 #define ELE_NODESCAN  0b00000010
