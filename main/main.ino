fa1c431303688f0264400ff77c40c7c714750132
Masafumim Horimoto on smena8 <masafumi.horimoto@gmail.com>
2026-02-07 15:09:33 +0900

    一応動く

--- main/main.ino
+++ main/main.ino
@@ -21,7 +21,7 @@ void get_mcusr(void) {
     wdt_disable();
 }
 
-char *pgname = "M304 ABCO2-008 ";
+char *pgname = "M304 ABCO2-009 ";
 
 #define ELE_UECS      0b00000001
 #define ELE_NODESCAN  0b00000010
